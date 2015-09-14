afbbe876a40217302714c3d8536ee6377aaebcbf
Mike Houghton <mike@Mikes-MacBook-Air.local>
2015-09-14 19:10:34 +0100

    Added

---
+++ FSR.hs
@@ -0,0 +1,72 @@
+{-# OPTIONS_GHC -Wall #-}
+{-# OPTIONS_GHC -fno-warn-incomplete-uni-patterns #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE FlexibleInstances #-}
+import Streams
+import Data.Monoid
+import Numeric (showHex, showIntAtBase)
+
+data Bit = Zero | One  deriving (Show)
+
+xor :: Bit -> Bit -> Bit 
+xor Zero Zero = Zero
+xor One One   = Zero
+xor _ _       = One
+
+nott :: Bit -> Bit
+nott Zero = One
+nott One = Zero
+
+nxor :: Bit -> Bit -> Bit
+nxor b1 b2 = nott  (xor b1 b2)
+
+type FSR     = [Bit] 
+type TapList = [Int]
+type BitList = [Bit]
+
+taps :: FSR -> TapList -> BitList
+taps ( []) _  = [Zero]
+taps fsr taps = [ fsr !! i | i <- taps]
+
+mapFSR :: (Bit -> Bit) -> FSR -> FSR
+mapFSR _ ( []) =  []
+mapFSR f ( bits) =  (map f bits)
+
+
+shiftR :: FSR -> Bit  -> FSR
+shiftR [] b =  [b]
+shiftR bits b =   b : init bits
+
+asString :: FSR -> String
+asString fsr  = foldl (\acc x -> acc ++ bitAsStr x) "" fsr where
+	bitAsStr Zero = " 0 "
+	bitAsStr One  = " 1 "
+
+fromStr :: String -> FSR
+fromStr [] = []
+fromStr str = foldl (\acc x -> acc ++ f x) [] str where
+	f '1' = [One]
+	f '0' = [Zero]
+	f  _  = [One] 
+
+asInteger :: FSR -> Integer
+asInteger bits = sum [ (f (bits !! n) ) * 2^n | n <- [0.. (length bits) -1]] where
+	f Zero = 0
+	f One  = 1
+	
+out :: FSR -> Bit
+out [] = Zero
+out fsr = (fsr !! last) where last = (length fsr) -1
+
+latch :: FSR -> TapList -> FSR
+latch fsr tapList = shiftR fsr nb where 
+	nb =  foldl (\acc x -> xor x acc )  first rest where
+		(first:rest) = taps fsr tapList
+
+
+start :: FSR 
+--start = [ Zero, Zero, One, Zero, One, One, One, Zero,One, One, One, Zero,Zero, Zero, One, Zero, One, One, One, Zero,One, One, One, Zero, Zero,One, One, One, Zero]
+start = [One, Zero, One, One, One, Zero, One, One, One, Zero, One, Zero, One, One, One, One, One]
+
+--fsrStream :: Stream Integer
+fsrStream fsr = Cons (asInteger fsr') (fsrStream fsr') where fsr' = latch fsr [13,16]
---
+++ Streams.hs
@@ -0,0 +1,62 @@
+
+{-# OPTIONS_GHC -Wall #-}
+{-# OPTIONS_GHC -fno-warn-incomplete-uni-patterns #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE FlexibleInstances #-}
+module Streams where
+import Control.Applicative
+
+
+data Stream a = Cons a (Stream a) 
+
+instance Show a => Show (Stream a) where
+        show str = show $ take 50000 $ streamToList str
+
+instance Fractional (Stream Integer ) where
+        (/) a@(Cons a0 a') b@(Cons b0 b') = Cons (a0 `div` b0) $ streamMap ( `div` b0) (a' - b'* (a / b))
+
+instance Num (Stream Integer) where
+        fromInteger n = Cons n (streamRepeat 0)
+        negate s = streamMap (negate) s
+        (+) (Cons a1 s1) (Cons a2 s2) = Cons (a1 + a2) ((+) s1 s2)
+        (*) (Cons a0 a') b@(Cons b0 b') = Cons (a0 * b0) ((mulConst a0 b') + (a' * b))
+
+instance Functor (Stream) where
+        fmap f (Cons x sx) = (Cons (f x)) (fmap f sx)
+
+instance Applicative Stream where
+        pure x = Cons x (streamRepeat x)
+        (Cons f sf) <*> (Cons x sx) = (Cons (f x)) (sf <*> sx)
+
+mulConst :: Integer -> Stream Integer -> Stream Integer
+mulConst n  = streamMap (*n) 
+
+divConst n = streamMap (/n)
+
+streamToList :: Stream a -> [a]
+streamToList (Cons x stream) = [x] ++ streamToList stream
+
+streamRepeat :: a -> Stream a
+streamRepeat n = Cons n (streamRepeat n)
+
+streamMap :: (a -> b) -> Stream a -> Stream b
+streamMap f ( Cons x stream ) = Cons (f x) (streamMap f stream)
+
+streamFromSeed :: (a -> a) -> a -> Stream a
+streamFromSeed f a = Cons fa (streamFromSeed f fa) where
+        fa = f a
+
+
+interleave :: Stream a -> Stream a -> Stream a
+interleave (Cons a s1) (Cons b s2) = Cons a (Cons b (interleave s1 s2))
+
+
+dropAlt :: Stream a -> Stream a
+dropAlt (Cons _ (Cons b s)) = Cons b (dropAlt s)
+
+
+nats :: Stream Integer
+nats =  streamFromSeed (+1) 0
+
+
+
